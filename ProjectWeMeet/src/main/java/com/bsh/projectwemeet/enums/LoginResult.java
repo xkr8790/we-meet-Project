7dfb23d9dfa6ce878e14c66ae88722f6f8b193d0
SunghyunBack <128347334+SunghyunBack@users.noreply.github.com>
2023-06-14 21:57:11 +0900

    Delete ProjectWeMeet directory

--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/enums/LoginResult.java
+++
@@ -1,8 +0,0 @@
-package com.bsh.projectwemeet.enums;
-
-public enum LoginResult {
-//    1.실패 2.중복 3.성공
-    FAILURE,
-    FAILURE_SUSPENDED,
-    SUCCESS
-}
--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/enums/VerifyRecovercontactCodeResult.java
+++
@@ -1,7 +0,0 @@
-package com.bsh.projectwemeet.enums;
-
-public enum VerifyRecovercontactCodeResult {
-    FAUILURE,
-    FAILURE_EXPIRED,
-    SUCCESS
-}
