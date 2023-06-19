9cdf42524962b49f320ab68398098e14c76d54f7
YSH <tjrgns21@naver.com>
2023-06-19 10:36:29 +0900

    ysh

--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/utils/NCloudUtil.java
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/utils/NCloudUtil.java
@@ -15,81 +15,65 @@ import java.util.Base64;
 import java.util.Date;
 
 public class NCloudUtil {
-    //     문자로 인증 보내는것
-    private static final String URL_PREFIX = "";
-    private static final String ACCESS_KEY = "";
-    private static final String SECRET_KEY = "";
-    // 인증키는 Ncloud 홈페이지 마이페이지 인증키에서 가져오기
-//    시크릿 키는 (보기) 해당란의 내용을 넣기
-    private static final String SERVICE_ID = "ncp:sms:kr:307475261582:portfolio";
-// Ncloud 홈페이지 콘솔 Simple & Easy Notification Service 의 project 에서 생성한 protfolio에서 서비스 id 복사하기
-
-    private static String generateSignature(String requestMethod, String requestUrl, long timestamp) {
-//        requestUrl = requestUrl.replace(NCloudUtil.URL_PREFIX, "");
-//        28번 라인은 도메인을 없애는 코드
+    private static final String URL_PREFIX ="https://sens.apigw.ntruss.com";
+    private static final String ACCESS_KEY = "EXOIpCy7nze8IR2mszbF";
+    private static final String SECRET_KEY = "KN8GfoCwJbuDBm97F7cDol4TW0IPNmjWbBsf7RYe";
+    private static final String SERVICE_ID="ncp:sms:kr:307474267728:portfolio";
+    private static String generateSignature(String requestMethod, String requestUrl, long timestamp){
+        requestUrl = requestUrl.replace(NCloudUtil.URL_PREFIX,"");
         String signature = String.format("%s %s\n%d\n%s",
                 requestMethod,
-                requestUrl.replace(NCloudUtil.URL_PREFIX, ""),
+                requestUrl.replace(NCloudUtil.URL_PREFIX,""),
                 timestamp,
                 NCloudUtil.ACCESS_KEY);
-        SecretKeySpec spec = new SecretKeySpec(NCloudUtil.SECRET_KEY.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-//        HmacSHA256은 알고리즘을 가리킨다.
+        SecretKeySpec spec = new SecretKeySpec(NCloudUtil.SECRET_KEY.getBytes(StandardCharsets.UTF_8),"HmacSHA256");
         try{
             Mac mac = Mac.getInstance("HmacSHA256");
             mac.init(spec);
             byte[] hmacBytes = mac.doFinal(signature.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(hmacBytes);
         } catch (NoSuchAlgorithmException | InvalidKeyException e){
-//            getInstace와 inti 때문에  다음과 같이 한번에 예외 처리를 하겠다.
             throw new RuntimeException(e);
         }
     }
-
-    //    아래 코드는 http통신을 하기 위한 코드이다.
-    //                             받는사람      내용
-    public static void sendSms(String to, String content) {
+    public static void sendSms(String to, String content){
         final String requestMethod = "POST";
-        final String requestUrl = String.format("%s/sms/v2/services/%s/messages",
-                NCloudUtil.URL_PREFIX,
-                NCloudUtil.SERVICE_ID);
-//        안의 홈페이지는
-        final JSONObject requestBody = new JSONObject(){{
-            put("type", "SMS");
-            put("contentType", "COMM");
-//            put("countryCode", "82");
-            put("from", "");
-            put("content", content);
-            put("messages", new JSONArray() {{
+        final String requestUrl = String.format("%s/sms/v2/services/%s/messages",NCloudUtil.URL_PREFIX,NCloudUtil.SERVICE_ID);
+        final JSONObject requestBody = new JSONObject() {{
+            put("type","SMS");
+            put("contentType","COMM");
+            put("countryCode","82");
+            put("from","01092646977");
+            put("content",content);
+            put("messages", new JSONArray(){{
                 put(new JSONObject(){{
-                    put("to", to);
+                    put("to",to);
                 }});
             }});
-
         }};
         try {
             long timestamp = new Date().getTime();
             String signature = NCloudUtil.generateSignature(requestMethod, requestUrl, timestamp);
-
             URL url = new URL(requestUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod(requestMethod);
-            connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-            connection.setRequestProperty("x-ncp-apigw-timestamp", String.valueOf(timestamp));
-            connection.setRequestProperty("x-ncp-iam-access-key", NCloudUtil.ACCESS_KEY);
-            connection.setRequestProperty("x-ncp-apigw-signature-v2", signature);
+            connection.setRequestProperty("Content-Type","application/json; charset=utf-8");
+            connection.setRequestProperty("x-ncp-apigw-timestamp",String.valueOf(timestamp));
+            connection.setRequestProperty("x-ncp-iam-access-key",NCloudUtil.ACCESS_KEY);
+            connection.setRequestProperty("x-ncp-apigw-signature-v2",signature);
             connection.setDoOutput(true);
-
-            try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()))){
+            try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()))) {
                 writer.write(requestBody.toString());
                 writer.flush();
             }
-            try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 String response = reader.readLine();
                 System.out.println(response);
             }
-        }catch(IOException e){
+        } catch (IOException e){
             throw new RuntimeException(e);
         }
+
     }
 
     private NCloudUtil() {
