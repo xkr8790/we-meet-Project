a6dcaa2b2f32c092ed7211a3eb3fa4ea74aa4b21
YSH <tjrgns21@naver.com>
2023-06-21 12:34:53 +0900

    ysh 06 21 게시글(write) 지도 추가

---
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/controllers/articleController.java
@@ -0,0 +1,23 @@
+package com.bsh.projectwemeet.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class articleController {
+
+
+    @RequestMapping(value = "write",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getWrite(){
+        ModelAndView modelAndView = new ModelAndView("home/write");
+        return modelAndView;
+    }
+
+}
