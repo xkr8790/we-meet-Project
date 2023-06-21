68ba443627b85889fe75d1b73ef4e69a85e04462
YSH <tjrgns21@naver.com>
2023-06-21 15:56:43 +0900

    ysh 06 21 게시글(write) 지도 추가 완료

--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/controllers/writeController.java
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/controllers/writeController.java
@@ -4,12 +4,11 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/")
-public class articleController {
+public class writeController {
 
 
     @RequestMapping(value = "write",
---
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/write.js
@@ -0,0 +1,1 @@
+const writeForm = document.getElementById('writeForm');
