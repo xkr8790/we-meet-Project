0191b42f380d99d6967b2087982e62891c63f181
MinBumkim1901 <chngdch3@naver.com>
2023-06-22 16:46:04 +0900

    게시판 작성폼 만드는중

--- ProjectWeMeet/src/main/resources/static/resources/scripts/Article-Write.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/Article-Write.js
@@ -1,3 +1,4 @@
+const ArticleWrite = document.getElementById('Article-Write');
 ClassicEditor
     .create(document.querySelector('#editor'), {
         language: 'ko', //언어설정
@@ -7,12 +8,49 @@ ClassicEditor
         console.error(error);
     });
 
-const ArticleWrite = document.getElementById('Article-Write');
+const articleTag = document.querySelector('.article-tag');
+let Tag = document.querySelector('.tag');
+
+
+articleTag.addEventListener('click', function () {
+    const existingTags = document.querySelectorAll('.tag');
+
+    if (existingTags.length < 6) {
+        const newTag = Tag.cloneNode(true);
+        newTag.style.background = '#eff0f2';
+        newTag.style.borderRadius = '0.2rem';
+        newTag.style.fontSize = '0.8rem';
+        newTag.style.height = '30px';
+        newTag.style.display = 'flex';
+        newTag.style.alignItems = 'center';
+        newTag.style.padding = '0 9px';
+        newTag.style.marginLeft = '0.5rem';
+
+        articleTag.appendChild(newTag);
+
+        newTag.focus();
+    }
+});
+
 
 ArticleWrite.onsubmit = e => {
     e.preventDefault();
-    if (ArticleWrite['text'].value === ''){
-        alert('내용이 비었다');
+    if (ArticleWrite['ArticleTitle'].value === '') {
+        alert('제목이 비었다');
+        ArticleWrite['ArticleTitle'].focus();
+        ArticleWrite['ArticleTitle'].select();
+        return;
     }
-
+    if (ArticleWrite['content'].value === '') {
+        alert('내용이 비었다');
+        ArticleWrite['content'].focus();
+        ArticleWrite['content'].select();
+        return;
+    }
+    if (ArticleWrite['tag'].value === '') {
+        alert('태그가 비어있습니다 태그를 적어주세요');
+        ArticleWrite['tag'].focus();
+        return;
+    }
 }
+
