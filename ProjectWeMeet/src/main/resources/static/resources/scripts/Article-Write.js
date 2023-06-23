97f2b6dac3de747427df698ec85a7dcc5aa9d500
MinBumkim1901 <chngdch3@naver.com>
2023-06-23 14:00:35 +0900

    게시판 작성폼 만드는중

--- ProjectWeMeet/src/main/resources/static/resources/scripts/Article-Write.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/Article-Write.js
@@ -8,49 +8,59 @@ ClassicEditor
         console.error(error);
     });
 
-const articleTag = document.querySelector('.article-tag');
-let Tag = document.querySelector('.tag');
+const ArticleTag = document.querySelector('.article-tag');
 
+let tagCounter = 0; //전역변수 태그카운터
 
-articleTag.addEventListener('click', function () {
-    const existingTags = document.querySelectorAll('.tag');
-
-    if (existingTags.length < 6) {
-        const newTag = Tag.cloneNode(true);
-        newTag.style.background = '#eff0f2';
-        newTag.style.borderRadius = '0.2rem';
-        newTag.style.fontSize = '0.8rem';
-        newTag.style.height = '30px';
-        newTag.style.display = 'flex';
-        newTag.style.alignItems = 'center';
-        newTag.style.padding = '0 9px';
-        newTag.style.marginLeft = '0.5rem';
-
-        articleTag.appendChild(newTag);
-
-        newTag.focus();
+ArticleTag.addEventListener('click', function () {
+    if (tagCounter >= 7) {
+        return; // 최대 태그 제한(7개)에 도달했을 경우 클릭해도 동작하지 않음
     }
-});
 
+    const existingTags = ArticleTag.querySelectorAll('.tag');
+    for (let i = 0; i < existingTags.length; i++) {
+        const tag = existingTags[i];
+        if (tag.textContent.trim() === '#') {
+            return; // 이미 생성된 태그가 있고, 해당 태그의 내용이 비어있으면 클릭해도 동작하지 않음
+        }
+    }
+
+    const Tag = document.createElement('div');
+    Tag.textContent = '#'; //처음 생성시 #추가
+    Tag.classList.add('tag'); //처음생성시 tag클래스 추가
+    Tag.contentEditable = true; //div 글자입력 가능
+    ArticleTag.appendChild(Tag); //article-tag의 자식으로 들어감
+
+    tagCounter++; // 태그 생성 횟수 증가
+});
 
 ArticleWrite.onsubmit = e => {
     e.preventDefault();
-    if (ArticleWrite['ArticleTitle'].value === '') {
-        alert('제목이 비었다');
-        ArticleWrite['ArticleTitle'].focus();
-        ArticleWrite['ArticleTitle'].select();
+    if(ArticleWrite['ArticleTitle'].value === ''){
+        alert('제목이 비어있습니다');
         return;
     }
-    if (ArticleWrite['content'].value === '') {
-        alert('내용이 비었다');
-        ArticleWrite['content'].focus();
-        ArticleWrite['content'].select();
+    if(ArticleWrite['content'].value === ''){
+        alert('내용이 비어있습니다');
         return;
     }
-    if (ArticleWrite['tag'].value === '') {
-        alert('태그가 비어있습니다 태그를 적어주세요');
-        ArticleWrite['tag'].focus();
+    if(tagCounter === 0){
+        alert('태그가 비어있습니다');
         return;
     }
 }
 
+// 파일 선택 시 미리보기 기능 구현
+const photoUpload = document.getElementById('photo-upload');
+const thumbnailPreview = document.getElementById('thumbnail-preview');
+
+photoUpload.addEventListener('change', function () {
+    const file = photoUpload.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            thumbnailPreview.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
