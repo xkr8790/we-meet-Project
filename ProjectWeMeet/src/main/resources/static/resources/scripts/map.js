50bbeafa00bd1c85e3e015b0b500bff6d843c1d1
YSH <tjrgns21@naver.com>
2023-06-26 16:43:59 +0900

    ysh 06 26

--- ProjectWeMeet/src/main/resources/static/resources/scripts/map.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/map.js
@@ -103,20 +103,25 @@ function displayPlaces(places) {
         // 해당 장소에 인포윈도우에 장소명을 표시합니다
         // mouseout 했을 때는 인포윈도우를 닫습니다
         (function(marker, title) {
+
             kakao.maps.event.addListener(marker, 'mouseover', function() {
                 displayInfowindow(marker, title);
+
             });
 
             kakao.maps.event.addListener(marker, 'mouseout', function() {
                 infowindow.close();
+
             });
 
             itemEl.onmouseover =  function () {
                 displayInfowindow(marker, title);
+
             };
 
             itemEl.onmouseout =  function () {
                 infowindow.close();
+
             };
         })(marker, places[i].place_name);
 
@@ -254,6 +259,10 @@ function displayInfowindow(marker, title) {
 
     infowindow.setContent(content);
     infowindow.open(map, marker);
+
+
+
+
 }
 
 // 검색결과 목록의 자식 Element를 제거하는 함수입니다
