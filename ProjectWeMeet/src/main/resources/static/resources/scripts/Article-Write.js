be0c607ef6a98d1c4f91c932a07e59a2ad2cd733
MinBumkim1901 <chngdch3@naver.com>
2023-06-26 11:04:06 +0900

    게시판 작성폼 만드는중

--- ProjectWeMeet/src/main/resources/static/resources/scripts/Article-Write.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/Article-Write.js
@@ -14,6 +14,12 @@ const explainTag = document.querySelector('.explainTag'); //설명
 let tagCounter = 0; //전역변수 태그카운터
 
 ArticleTag.addEventListener('click', function () {
+
+
+    explainTag.addEventListener('click', function () {
+        explainTag.style.display = 'none';
+    })
+
     explainTag.style.display = 'none'; // 처음에 태그 몇 개 적는지 설명하는 div
     if (tagCounter >= 5) {
         return; // 최대 태그 제한(5개) 그 이상 클릭해도 작동X
@@ -27,32 +33,82 @@ ArticleTag.addEventListener('click', function () {
     }
 
     const Tag = document.createElement('div');
+
     Tag.textContent = '#'; // 처음 생성시 # 추가
     Tag.classList.add('tag'); // 처음 생성시 tag 클래스 추가
     Tag.contentEditable = true; // div 글자 입력 가능
+    Tag.style.userSelect = 'none'; // 일반적인 방식
     ArticleTag.appendChild(Tag); // article-tag의 자식으로 들어감
+
+    Tag.addEventListener('mousedown', function(event) {
+        event.preventDefault();
+    });
+
+    if (Tag.textContent.charAt(0) === '#') {
+        Tag.focus();
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.setStart(Tag.firstChild, 1); // # 다음 문자에 커서 설정
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+
 
     tagCounter++; // 태그 생성 횟수 증가
 
-    Tag.addEventListener('keydown', function(event) {
-        if (event.key === '#') {
+
+    Tag.addEventListener('click', function (event) {
+        if (Tag !== event.target) {
+            event.stopPropagation();
+            return;
+        }
+        const trimmedText = Tag.textContent.trim().replace(/\s+/g, '');
+        if (trimmedText.length > 1) {
+            event.stopPropagation(); // 내용이 있는 경우 클릭 이벤트 전파 중지
+        } else if (Tag.classList.contains('TA')) {
+            Tag.contentEditable = true; // 새로운 태그를 클릭했을 때 입력 가능하도록 설정
+        } else {
+            event.stopPropagation(); // 기존 태그의 클릭 이벤트 전파 중지
+        }
+    });
+
+    function getCaretPosition(element) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.setStart(element.firstChild, 0);
+        range.setEnd(selection.focusNode, selection.focusOffset);
+        return range.toString().length;
+    }
+
+
+    Tag.addEventListener('keydown', function (event) {
+        const key = event.key;
+        const trimmedText = Tag.textContent.trim().replace(/\s+/g, '');
+
+        if (key === 'ArrowLeft') {
+            const caretPosition = getCaretPosition(Tag);
+            if (caretPosition <= 1 && trimmedText.charAt(0) === '#') {
+                event.preventDefault();
+            }
+        } else if (key === 'ArrowRight') {
+            const caretPosition = getCaretPosition(Tag);
+            if (caretPosition === trimmedText.length && trimmedText.charAt(0) === '#') {
+                event.preventDefault();
+            }
+        } else if (key === 'Backspace') {
+            if (trimmedText === '#') {
+                event.preventDefault();
+            }
+        } else if (key === '#') {
             event.preventDefault();
-        } else if (event.key === 'Backspace' && Tag.textContent === '#') {
-            event.preventDefault();
-        } else if (event.key === 'Enter') {
-            event.preventDefault(); // Prevent line break on Enter key
-        } else if (Tag.innerHTML.length >= 11) {
+        } else if (key === 'Enter') {
             event.preventDefault();
         }
     });
-    // keyDonw즉 사용자가 Tag(div)안에 무언갈 쓸때 일어나는 이벤트이다.
-    //결론적으로 #은 태그의 시작이니 지우거나 쓰지를 못하면서
-    // Enter 줄바꿈 방지하고 태그를 많이쓰는걸방지하기 위해 11자만 쓰는게 가능
-
-    Tag.addEventListener('click', function() {
-        // Tag.style.color = '#979797'; // 클릭시 색상을 빨간색으로 변경
-    });
 });
+
+
 
 
 
