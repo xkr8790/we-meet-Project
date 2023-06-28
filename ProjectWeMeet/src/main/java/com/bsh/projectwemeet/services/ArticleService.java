7b1e0c6814c7f03a9a8a4ef555b2836ad887fb30
MinBumkim1901 <chngdch3@naver.com>
2023-06-28 17:04:58 +0900

    게시판폼수정

---
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/services/ArticleService.java
@@ -0,0 +1,10 @@
+package com.bsh.projectwemeet.services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+
+
+}
