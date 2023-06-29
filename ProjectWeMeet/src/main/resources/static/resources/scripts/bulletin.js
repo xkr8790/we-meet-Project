fd4847f3de92fcd55aacfc569c0626fc00b913cf
SungHyun Back <sunghyun960205@gmail.com>
2023-06-29 14:47:15 +0900

    bulletin 꾸미기

---
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/bulletin.js
@@ -0,0 +1,63 @@
+const list = document.querySelector('.container');
+const listScrollWidth = list.scrollWidth;
+const listClientWidth = list.clientWidth;
+
+let startX = 0;
+let nowX = 0;
+let endX = 0;
+let listX = 0;
+let isDragging = false;
+
+const getClientX = (e) => {
+    const isTouches = e.touches ? true : false;
+    return isTouches ? e.touches[0].clientX : e.clientX;
+};
+
+const getTranslateX = () => {
+    return parseInt(getComputedStyle(list).transform.split(/[^\-0-9]+/g)[5]);
+};
+
+const setTranslateX = (x) => {
+    list.style.transform = `translateX(${x}px)`;
+};
+
+const onScrollStart = (e) => {
+    startX = getClientX(e);
+    isDragging = true;
+    document.addEventListener('mousemove', onScrollMove);
+    document.addEventListener('touchmove', onScrollMove);
+    document.addEventListener('mouseup', onScrollEnd);
+    document.addEventListener('touchend', onScrollEnd);
+};
+
+const onScrollMove = (e) => {
+    if (!isDragging) return;
+    nowX = getClientX(e);
+    const v = listX + nowX - startX;
+    if (v > 0) return;
+    if (v <  -(227* list.childElementCount)) return;
+    console.log(v);
+    setTranslateX(v);
+};
+
+const bindEvents = () => {
+    list.addEventListener('mousedown', onScrollStart);
+    list.addEventListener('touchstart', onScrollStart);
+};
+
+const onScrollEnd = (e) => {
+    if (!isDragging) return;
+    isDragging = false;
+    endX = getClientX(e);
+    listX = getTranslateX();
+    document.removeEventListener('mousemove', onScrollMove);
+    document.removeEventListener('touchmove', onScrollMove);
+    document.removeEventListener('mouseup', onScrollEnd);
+    document.removeEventListener('touchend', onScrollEnd);
+
+
+};
+
+bindEvents();
+
+
