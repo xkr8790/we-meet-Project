a5f0fc075a46f30d68fefaf8af77d0d124cd1b64
MinBumkim1901 <chngdch3@naver.com>
2023-07-03 22:00:53 +0900

    게시판 작성폼 최종본

--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/mappers/ArticleMapper.java
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/mappers/ArticleMapper.java
@@ -2,10 +2,19 @@ package com.bsh.projectwemeet.mappers;
 
 import com.bsh.projectwemeet.entities.ArticleEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ArticleMapper {
+
     int insertArticle(ArticleEntity articles);
+
+    ArticleEntity[] selectAll();
+
+    ArticleEntity selectArticleByIndex(@Param(value = "index") int index);
+
+    int updateArticle(ArticleEntity article);
+
 
 
 
