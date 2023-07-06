d66c199351c85bbc8b1b3b5b9dc856f5a0b2f791
YSH <tjrgns21@naver.com>
2023-07-06 11:36:40 +0900

    ysh 07 06

--- ProjectWeMeet/src/main/resources/static/resources/scripts/bulletinMap.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/bulletinMap.js
@@ -1,16 +1,16 @@
-var mapContainer = document.getElementById('map'),
+const mapContainer = document.getElementById('map'),
     mapOption = {
         center: new kakao.maps.LatLng(bulletinForm['lat'].value,bulletinForm['lng'].value),
         level: 3
     };
 
-var map = new kakao.maps.Map(mapContainer, mapOption);
+const map = new kakao.maps.Map(mapContainer, mapOption);
 
 
-var markerPosition  = new kakao.maps.LatLng(bulletinForm['lat'].value,bulletinForm['lng'].value);
+const markerPosition  = new kakao.maps.LatLng(bulletinForm['lat'].value,bulletinForm['lng'].value);
 
 
-var marker = new kakao.maps.Marker({
+const marker = new kakao.maps.Marker({
     position: markerPosition
 });
 
