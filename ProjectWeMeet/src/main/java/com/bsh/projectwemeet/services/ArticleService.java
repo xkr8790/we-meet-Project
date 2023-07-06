988da99269d314577518f93e90e632fa13348350
MinBumkim1901 <chngdch3@naver.com>
2023-07-06 11:38:35 +0900

    리다이렉트로 게시판 작성시 게시판 바로가고 참여버튼시 중복 만드는중입니다

--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/services/ArticleService.java
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/services/ArticleService.java
@@ -1,12 +1,15 @@
 package com.bsh.projectwemeet.services;
 
 import com.bsh.projectwemeet.entities.ArticleEntity;
+import com.bsh.projectwemeet.entities.ParticipantsEntity;
 import com.bsh.projectwemeet.entities.UserEntity;
+import com.bsh.projectwemeet.enums.InsertParticipate;
 import com.bsh.projectwemeet.mappers.ArticleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 @Service
 public class ArticleService {
@@ -45,21 +48,41 @@ public class ArticleService {
         //결과적으로 삭제되지않거나
     }
 
-    public boolean Participate(int index) {
+    public InsertParticipate Participate(int index, ParticipantsEntity participants, HttpSession session) {
 
         ArticleEntity article = this.articleMapper.selectArticleByIndex(index);
+        UserEntity loginUser = (UserEntity) session.getAttribute("user");
 
-        if (article != null && !article.isDeleted()) {
-            // 게시글이 존재하고 삭제되지 않은 경우에만 처리합니다.
-            if (article.getParticipation() <= article.getLimitPeople()) {
-                article.setParticipation(article.getParticipation() + 1);
-                // 게시글의 조회수(view)를 1 증가시킵니다.
-                this.articleMapper.updateArticle(article);
-            }
-        } else {
-            return false;
+        if(loginUser.getEmail() == null || article == null){
+            return InsertParticipate.FAILURE;
+        } //로그인한유저가 없을시 실패
+
+        if(article.getParticipation() >= article.getLimitPeople()){
+            return InsertParticipate.FAILURE;
+        } //제한인원을 초과해서 입력시 실패
+
+        if(participants.isCheckParticipationStatus()){
+            return InsertParticipate.FAILURE;
         }
-        return this.articleMapper.patchArticle(article)>0;
+
+        article.setParticipation(article.getParticipation() + 1);
+        this.articleMapper.updateParticipate(article);
+
+        participants.setArticleIndex(article.getIndex())
+                .setCreatedAt(new Date())
+                .setEmail(loginUser.getEmail())
+                .setCheckParticipationStatus(true);
+
+        return this.articleMapper.insertParticipants(participants) > 0
+                ? InsertParticipate.SUCCESS
+                : InsertParticipate.FAILURE;
+    }
+
+    public boolean SelectParticipants(int index, ParticipantsEntity participants, HttpSession session) {
+        ArticleEntity article = this.articleMapper.selectArticleByIndex(index);
+        UserEntity loginUser = (UserEntity) session.getAttribute("user");
+
+        return articleMapper.selectParticipants(article.getIndex()) > 0;
     }
 
     public boolean deleteByIndex(int index){
@@ -71,9 +94,6 @@ public class ArticleService {
         return this.articleMapper.selectArticleByPatchIndex(index);
     }
 
-    public ArticleEntity patchArticle(int index){
-        return null;
-    }
 
 
 }
