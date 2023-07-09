6fd9461fb10bd54f7d02b435c0797a8119fa4992
xkr87 <xkr87901@naver.com>
2023-07-09 22:01:06 +0900

    test

--- ProjectWeMeet/src/main/resources/static/resources/scripts/profile.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/profile.js
@@ -4,7 +4,6 @@ const step1 = document.querySelector('.step-1');
 const saveButton = document.querySelector('.save');
 const passwordInputStep1 = step1.querySelector('._object-input');
 const checkPasswordButton = step1.querySelector('.checkPassword');
-const profile = document.getElementById('mainContainer');
 
 settingButton.addEventListener('click', () => {
     popup.classList.add('step-1');
@@ -13,6 +12,7 @@ settingButton.addEventListener('click', () => {
     step1.style.display = 'block';
     popup['_object-input'].focus();
 });
+
 
 saveButton.addEventListener('click', () => {
     popup.style.display = 'none';
@@ -24,6 +24,7 @@ HTMLInputElement.prototype.focusAndSelect = function () {
 };
 
 
+
 popup.onsubmit = e => {
     e.preventDefault();
 
@@ -33,41 +34,41 @@ popup.onsubmit = e => {
         alert('비밀번호를 입력해 주세요.');
         return false;
     }
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
-    formData.append('checkPassword', popup['checkPassword'].value);
-    xhr.open('POST', `/checkPassword`);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                const responseObject = JSON.parse(xhr.responseText);
-                switch (responseObject.result) {
-                    case 'failure' :
-                        alert('비밀번호가 일치하지 않습니다.');
-                        break;
-                    case 'success':
-                        alert('확인되었습니다.');
-                        popup.classList.remove('step-1');
-                        popup.classList.add('step-2');
-                        break;
-                    default:
-                        alert('서버가 알 수 없는 응답을 반환했습니다.');
-                }
-            } else {
-                alert('서버에 연결할 수 없습니다. 다시 시도해 주세요.');
-            }
-        }
-    };
-    xhr.send(formData);
+    // const xhr = new XMLHttpRequest();
+    // const formData = new FormData();
+    // formData.append('checkPassword', popup['checkPassword'].value);
+    // xhr.open('POST', `/checkPassword`);
+    // xhr.onreadystatechange = () => {
+    //     if (xhr.readyState === XMLHttpRequest.DONE) {
+    //         if (xhr.status >= 200 && xhr.status < 300) {
+    //             const responseObject = JSON.parse(xhr.responseText);
+    //             switch (responseObject.result) {
+    //                 case 'failure' :
+    //                     alert('비밀번호가 일치하지 않습니다.');
+    //                     break;
+    //                 case 'success':
+    //                     alert('확인되었습니다.');
+    //                     popup.classList.remove('step-1');
+    //                     popup.classList.add('step-2');
+    //                     break;
+    //                 default:
+    //                     alert('서버가 알 수 없는 응답을 반환했습니다.');
+    //             }
+    //         } else {
+    //             alert('서버에 연결할 수 없습니다. 다시 시도해 주세요.');
+    //         }
+    //     }
+    // };
+    // xhr.send(formData);
 
-    // if (password === '1234') {
-    //     popup.classList.remove('step-1');
-    //     popup.classList.add('step-2');
-    // } else {
-    //     alert('비밀번호가 올바르지 않습니다.');
-    //     popup['_object-input'].value = '';
-    //     popup['_object-input'].focus();
-    // }
+    if (password === '1234') {
+        popup.classList.remove('step-1');
+        popup.classList.add('step-2');
+    } else {
+        alert('비밀번호가 올바르지 않습니다.');
+        popup['_object-input'].value = '';
+        popup['_object-input'].focus();
+    }
 };
 
 popup['save'].addEventListener('click', () => {
@@ -76,38 +77,32 @@ popup['save'].addEventListener('click', () => {
     alert('변경된 사항이 저장되었습니다.');
 });
 
-// profile.profilePreview = profile.querySelector('[rel="profilePreview"]');
-// profile.profile1 = profile.querySelector('[rel="profile1"]');
-//
-// profile['profile'].onchange = function (e) {
-//     e.preventDefault();
-//
-//     if (profile['profile'].files.length === 0) {
-//         return;
-//     }
-//     const fileReader = new FileReader();
-//     fileReader.onload = function (data) {
-//         profile['profile1'].src = '';
-//         profile['profile1'].style.backgroundImage = `url("${data.target.result}")`;
-//     }
-//     fileReader.readAsDataURL(profile['profile'].files[0]);
-//     alert('프로필 사진이 변경되었습니다.');
-//
-//     const xhr = new XMLHttpRequest();
-//     const formData = new FormData();
-//
-//     formData.append('thumbnailMultipart', profile['profile'].files[0]);
-//
-//     xhr.open('POST', '/profile/profileImage');
-//     xhr.onreadystatechange = () => {
-//         if (xhr.readyState === XMLHttpRequest.DONE) {
-//             if (xhr.status >= 200 && xhr.status < 300) {
-//                 alert('섹');
-//             } else {
-//                 alert('ㅋㅋ');
-//             }
-//         }
-//     };
-//     xhr.send(formData);
-// };
-//
+var changeProfile =document.querySelector('.change_profile');
+const profileF = popup.querySelector('[rel="profileF"]');
+
+changeProfile.addEventListener('change', function (event) {
+    event.preventDefault();
+
+    popup['change_profile'].files[0] = event.target.files[0];
+    if (popup['change_profile'].files.length === 0) {
+        return;
+    }
+    var fileReader = new FileReader();
+    fileReader.onload = function (data) {
+        profileF.src = '';
+        profileF.style.backgroundImage = `url("${data.target.result}")`;
+    }
+    fileReader.readAsDataURL(popup['change_profile'].files[0]);
+    alert('프로필 사진이 변경되었습니다.');
+});
+
+const profileDelete = popup.querySelector('[rel="profileDelete"]');
+
+profileDelete.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    profileF.style.backgroundImage = `none`;
+    profileF.src = '/resources/images/profileImages/icons8-male-user-96.png';
+
+    alert('프로필 사진이 삭제되었습니다.');
+});
