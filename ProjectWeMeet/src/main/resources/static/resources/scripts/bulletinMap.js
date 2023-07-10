79cd4bfa7ed37928b978021ec19f5791b0d06354
YSH <tjrgns21@naver.com>
2023-07-10 12:32:27 +0900

    ysh 07 10

--- ProjectWeMeet/src/main/resources/static/resources/scripts/bulletinMap.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/bulletinMap.js
@@ -17,3 +17,19 @@ const marker = new kakao.maps.Marker({
 
 marker.setMap(map);
 
+var articlePlace = bulletinForm['mapPlace'].value;
+
+
+// var iwContent = '<div style="padding:5px;">' + articlePlace + '<br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+var iwContent = '<div style="padding:5px;">' + articlePlace; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
+    iwPosition = new kakao.maps.LatLng(bulletinForm['lat'].value,bulletinForm['lng'].value); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    position : iwPosition,
+    content : iwContent
+});
+
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker);
