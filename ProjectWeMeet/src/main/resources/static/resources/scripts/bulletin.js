c0dfea86c47afae94294a01c51d433a05295211d
YSH <tjrgns21@naver.com>
2023-07-13 13:25:02 +0900

    07 13 비로그인전 버튼 오류 주석처리

--- ProjectWeMeet/src/main/resources/static/resources/scripts/bulletin.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/bulletin.js
@@ -76,62 +76,62 @@ const finishButton = bulletinForm.querySelector('[name="finish"]');
 
 
 //게시글 수정 및 삭제 참여 리뷰파트//게시글 수정 및 삭제 참여 리뷰파트//게시글 수정 및 삭제 참여 리뷰파트//게시글 수정 및 삭제 참여 리뷰파트//
-deleteButton.addEventListener('click', e => {
-    e.preventDefault();
-    const index = deleteButton.dataset.index;
-    const confirmResult = confirm('삭제하시겠습니까?');
-    if (confirmResult === true) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('DELETE', `./read?index=${index}`);
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    const responseText = xhr.responseText;
-                    if (responseText === 'true') {
-                        alert('삭제되었습니다');
-                        location.href = '/article';
-                        return;
-                    } else {
-                        alert('작성한 사용자가 아니므로 삭제하지 못합니다');
-                        return;
-                    }
-                } else {
-                    alert('서버와 통신하지 못하였습니다.\n\n잠시 후 다시 시도해 주세요.');
-                    return;
-                }
-            }
-        };
-        xhr.send();
-    } else if (confirmResult === false) {
-        alert('삭제를 취소합니다');
-        return;
-    }
-}); //게시판 삭제
+// deleteButton.addEventListener('click', e => {
+//     e.preventDefault();
+//     const index = deleteButton.dataset.index;
+//     const confirmResult = confirm('삭제하시겠습니까?');
+//     if (confirmResult === true) {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('DELETE', `./read?index=${index}`);
+//         xhr.onreadystatechange = () => {
+//             if (xhr.readyState === XMLHttpRequest.DONE) {
+//                 if (xhr.status >= 200 && xhr.status < 300) {
+//                     const responseText = xhr.responseText;
+//                     if (responseText === 'true') {
+//                         alert('삭제되었습니다');
+//                         location.href = '/article';
+//                         return;
+//                     } else {
+//                         alert('작성한 사용자가 아니므로 삭제하지 못합니다');
+//                         return;
+//                     }
+//                 } else {
+//                     alert('서버와 통신하지 못하였습니다.\n\n잠시 후 다시 시도해 주세요.');
+//                     return;
+//                 }
+//             }
+//         };
+//         xhr.send();
+//     } else if (confirmResult === false) {
+//         alert('삭제를 취소합니다');
+//         return;
+//     }
+// }); //게시판 삭제
 
-patchButton.addEventListener('click', e => {
-    e.preventDefault();
-
-    const index = patchButton.dataset.index;
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `./patch?index=${index}`);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                const confirmResult = confirm('수정을 하시겠습니까?');
-                if (confirmResult === true) {
-                    location.href = `/article/patch?index=${index}`
-                } else if (confirmResult === false) {
-                    alert('수정을 취소합니다');
-                    return;
-                }
-            } else {
-                alert('작성한 사용자가 아니라 수정이 불가능합니다.');
-            }
-        }
-    };
-    xhr.send();
-}); //게시판 수정
+// patchButton.addEventListener('click', e => {
+//     e.preventDefault();
+//
+//     const index = patchButton.dataset.index;
+//
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', `./patch?index=${index}`);
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState === XMLHttpRequest.DONE) {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 const confirmResult = confirm('수정을 하시겠습니까?');
+//                 if (confirmResult === true) {
+//                     location.href = `/article/patch?index=${index}`
+//                 } else if (confirmResult === false) {
+//                     alert('수정을 취소합니다');
+//                     return;
+//                 }
+//             } else {
+//                 alert('작성한 사용자가 아니라 수정이 불가능합니다.');
+//             }
+//         }
+//     };
+//     xhr.send();
+// }); //게시판 수정
 
 
 ParticipateButton.addEventListener('click', e => {
@@ -280,22 +280,22 @@ Report.addEventListener('click', e => {
 
 //게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//
 
-finishButton.addEventListener('click', e => {
-    e.preventDefault();
-    const index = finishButton.dataset.index;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET',`/article/review?index=${index}`);
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status >=200 && xhr.status<300){
-                location.href = `/article/review?index=${index}`
-            }else{
-                alert('작성한 사용자가 아니라 수정이 불가능합니다.');
-            }
-        }
-    };
-    xhr.send();
-})
+// finishButton.addEventListener('click', e => {
+//     e.preventDefault();
+//     const index = finishButton.dataset.index;
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET',`/article/review?index=${index}`);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === XMLHttpRequest.DONE){
+//             if(xhr.status >=200 && xhr.status<300){
+//                 location.href = `/article/review?index=${index}`
+//             }else{
+//                 alert('작성한 사용자가 아니라 수정이 불가능합니다.');
+//             }
+//         }
+//     };
+//     xhr.send();
+// })
 
 //게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//게시글 수정 리뷰파트//
 
