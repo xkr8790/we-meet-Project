115996b2737a3044ea1fe0b6552b5e4571ff6c04
YSH <tjrgns21@naver.com>
2023-07-14 13:35:16 +0900

    07 14

--- ProjectWeMeet/src/main/resources/static/resources/scripts/bulletin.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/bulletin.js
@@ -361,7 +361,76 @@ function postComment(content, commentIndex, toFocus, refreshCommentAfter) {
     xhr.send(formData);
 }
 
-function refreshComment(position) {
+function createCommentElement(comment, positionClass) {
+    const div = document.createElement('div');
+    div.classList.add('comment');
+    div.classList.add(positionClass);
+
+    const headDiv = document.createElement('div');
+    headDiv.classList.add('comment-head');
+    const bodyDiv = document.createElement('div');
+    bodyDiv.classList.add('comment-body');
+
+    const dtDate = comment['createdAt'].split('T')[0];
+    const dtTime = comment['createdAt'].split('T')[1].split('.')[0];
+    headDiv.innerText = `${dtDate} ${dtTime}`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.innerText = '삭제';
+    deleteButton.dataset.commentIndex = comment.index;
+    deleteButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!confirm('정말로 해당 댓글을 삭제할까요?')) {
+            return;
+        }
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+        xhr.open('DELETE', '/comment?index=' + e.target.dataset.commentIndex);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    const responseObject = JSON.parse(xhr.responseText);
+                    switch (responseObject.result) {
+                        case 'failure':
+                            alert('알 수 없는 이유로 댓글을 삭제하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
+                            break;
+                        case 'failure_deleted':
+                            alert('이미 삭제된 댓글 입니다. 새로고침 후 확인해 주세요.');
+                            break;
+                        case 'failure_no_authority':
+                            alert('삭제할 수 있는 권한이 없습니다.');
+                            break;
+                        case 'failure_not_login':
+                            alert('로그인 후 다시 시도해 주세요');
+                            break;
+                        case 'success':
+                            refreshComment();
+                            break;
+                        default:
+                            alert('서버가 알 수 없는 응답을 반환 했습니다.');
+                    }
+                } else {
+                    alert('서버와 통신할 수 없습니다.');
+                }
+            }
+        };
+        xhr.send(formData);
+    });
+
+    if (comment['deleted']) {
+        bodyDiv.innerText = '삭제된 댓글입니다.';
+        bodyDiv.style.color = '#a0a0a0';
+        bodyDiv.style.fontStyle = 'italic';
+    } else {
+        bodyDiv.innerText = comment['content'];
+    }
+
+    div.append(headDiv, deleteButton, bodyDiv);
+    return div;
+}
+
+function refreshComment() {
     const articleIndex = bulletinForm['articleIndex'].value;
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `/comment?articleIndex=${articleIndex}`);
@@ -369,76 +438,23 @@ function refreshComment(position) {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status >= 200 && xhr.status < 300) {
                 const comments = JSON.parse(xhr.responseText);
-                commentContainer.innerHTML = ''; // Clear existing comments
+                const existingComments = Array.from(commentContainer.getElementsByClassName('comment'));
 
                 for (const comment of comments) {
-                    const div = document.createElement('div');
-                    const commentClass = position === 'right' ? 'comment-right' : 'comment-left';
+                    let isNewComment = true;
 
-                    div.classList.add(commentClass);
-                    const headDiv = document.createElement('div');
-                    headDiv.classList.add('comment-head');
-                    const bodyDiv = document.createElement('div');
-                    bodyDiv.classList.add('comment-body');
-
-                    const dtDate = comment['createdAt'].split('T')[0];
-                    const dtTime = comment['createdAt'].split('T')[1].split('.')[0];
-                    headDiv.innerText = `${dtDate} ${dtTime}`;
-
-                    const deleteButton = document.createElement('button');
-                    deleteButton.classList.add('delete-button');
-                    deleteButton.innerText = '삭제';
-                    deleteButton.dataset.commentIndex = comment.index;
-                    deleteButton.addEventListener('click', (e) => {
-                        e.preventDefault();
-                        if (!confirm('정말로 해당 댓글을 삭제할까요?')) {
-                            return;
+                    for (const existingComment of existingComments) {
+                        if (existingComment.dataset.commentIndex === comment.index.toString()) {
+                            isNewComment = false;
+                            break;
                         }
-                        const xhr = new XMLHttpRequest();
-                        const formData = new FormData();
-                        xhr.open('DELETE', '/comment?index=' + e.target.dataset.commentIndex);
-                        xhr.onreadystatechange = () => {
-                            if (xhr.readyState === XMLHttpRequest.DONE) {
-                                if (xhr.status >= 200 && xhr.status < 300) {
-                                    const responseObject = JSON.parse(xhr.responseText);
-                                    switch (responseObject.result) {
-                                        case 'failure':
-                                            alert('알 수 없는 이유로 댓글을 삭제하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
-                                            break;
-                                        case 'failure_deleted':
-                                            alert('이미 삭제된 댓글 입니다. 새로고침 후 확인해 주세요.');
-                                            break;
-                                        case 'failure_no_authority':
-                                            alert('삭제할 수 있는 권한이 없습니다.');
-                                            break;
-                                        case 'failure_not_login':
-                                            alert('로그인 후 다시 시도해 주세요');
-                                            break;
-                                        case 'success':
-                                            refreshComment();
-                                            break;
-                                        default:
-                                            alert('서버가 알 수 없는 응답을 반환 했습니다.');
-                                    }
-                                } else {
-                                    alert('서버와 통신할 수 없습니다.');
-                                }
-                            }
-                        };
-                        xhr.send(formData);
-                    });
-
-                    if (comment['deleted']) {
-                        bodyDiv.innerText = '삭제된 댓글입니다.';
-                        bodyDiv.style.color = '#a0a0a0';
-                        bodyDiv.style.fontStyle = 'italic';
-                        div.appendChild(bodyDiv);
-                    } else {
-                        bodyDiv.innerText = comment['content'];
-                        div.append(headDiv, deleteButton, bodyDiv);
                     }
 
-                    commentContainer.appendChild(div);
+                    if (isNewComment) {
+                        const positionClass = comment['deleted'] ? 'comment-left' : 'comment-right';
+                        const newComment = createCommentElement(comment, positionClass);
+                        commentContainer.appendChild(newComment);
+                    }
                 }
             } else {
                 alert('서버와 통신하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
@@ -456,76 +472,23 @@ function refreshCommentSuccessSame() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status >= 200 && xhr.status < 300) {
                 const comments = JSON.parse(xhr.responseText);
-                commentContainer.innerHTML = ''; // Clear existing comments
+                const existingComments = Array.from(commentContainer.getElementsByClassName('comment'));
 
                 for (const comment of comments) {
-                    const div = document.createElement('div');
-                    const commentClass = 'comment-right';
+                    let isNewComment = true;
 
-                    div.classList.add(commentClass);
-                    const headDiv = document.createElement('div');
-                    headDiv.classList.add('comment-head');
-                    const bodyDiv = document.createElement('div');
-                    bodyDiv.classList.add('comment-body');
-
-                    const dtDate = comment['createdAt'].split('T')[0];
-                    const dtTime = comment['createdAt'].split('T')[1].split('.')[0];
-                    headDiv.innerText = `${dtDate} ${dtTime}`;
-
-                    const deleteButton = document.createElement('button');
-                    deleteButton.classList.add('delete-button');
-                    deleteButton.innerText = '삭제';
-                    deleteButton.dataset.commentIndex = comment.index;
-                    deleteButton.addEventListener('click', (e) => {
-                        e.preventDefault();
-                        if (!confirm('정말로 해당 댓글을 삭제할까요?')) {
-                            return;
+                    for (const existingComment of existingComments) {
+                        if (existingComment.dataset.commentIndex === comment.index.toString()) {
+                            isNewComment = false;
+                            break;
                         }
-                        const xhr = new XMLHttpRequest();
-                        const formData = new FormData();
-                        xhr.open('DELETE', '/comment?index=' + e.target.dataset.commentIndex);
-                        xhr.onreadystatechange = () => {
-                            if (xhr.readyState === XMLHttpRequest.DONE) {
-                                if (xhr.status >= 200 && xhr.status < 300) {
-                                    const responseObject = JSON.parse(xhr.responseText);
-                                    switch (responseObject.result) {
-                                        case 'failure':
-                                            alert('알 수 없는 이유로 댓글을 삭제하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
-                                            break;
-                                        case 'failure_deleted':
-                                            alert('이미 삭제된 댓글 입니다. 새로고침 후 확인해 주세요.');
-                                            break;
-                                        case 'failure_no_authority':
-                                            alert('삭제할 수 있는 권한이 없습니다.');
-                                            break;
-                                        case 'failure_not_login':
-                                            alert('로그인 후 다시 시도해 주세요');
-                                            break;
-                                        case 'success':
-                                            refreshComment();
-                                            break;
-                                        default:
-                                            alert('서버가 알 수 없는 응답을 반환 했습니다.');
-                                    }
-                                } else {
-                                    alert('서버와 통신할 수 없습니다.');
-                                }
-                            }
-                        };
-                        xhr.send(formData);
-                    });
-
-                    if (comment['deleted']) {
-                        bodyDiv.innerText = '삭제된 댓글입니다.';
-                        bodyDiv.style.color = '#a0a0a0';
-                        bodyDiv.style.fontStyle = 'italic';
-                        div.appendChild(bodyDiv);
-                    } else {
-                        bodyDiv.innerText = comment['content'];
-                        div.append(headDiv, deleteButton, bodyDiv);
                     }
 
-                    commentContainer.appendChild(div);
+                    if (isNewComment) {
+                        const positionClass = comment['deleted'] ? 'comment-left' : 'comment-right';
+                        const newComment = createCommentElement(comment, positionClass);
+                        commentContainer.appendChild(newComment);
+                    }
                 }
             } else {
                 alert('서버와 통신하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
@@ -546,5 +509,5 @@ bulletinForm.onsubmit = function (e) {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    refreshComment('left');
+    refreshComment();
 });
