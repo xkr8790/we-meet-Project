0d8a3146fe57011e8aaa0211c21c0b7791977a04
ysh <tjrgns21@naver.com>
2023-08-07 02:35:03 +0900

    수정중

--- ProjectWeMeet/src/main/java/com/bsh/projectwemeet/ProjectWeMeetApplication.java
+++ ProjectWeMeet/src/main/java/com/bsh/projectwemeet/ProjectWeMeetApplication.java
@@ -3,8 +3,17 @@ package com.bsh.projectwemeet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ProjectWeMeetApplication {
+
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectWeMeetApplication.class, args);
