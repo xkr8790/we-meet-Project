86fd0cc1182e0e98ad4834e1dad67e20dac0770e
MinBumkim1901 <chngdch3@naver.com>
2023-08-08 13:39:37 +0900

    공지사항 반응형 끝

--- ProjectWeMeet/src/main/resources/static/resources/scripts/article.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/article.js
@@ -5,31 +5,33 @@ const articleLink750px = Article.querySelectorAll('.articleLink750px')
 
 let url = window.location.href; //현재 컴퓨터가 실행되고있는 URL을 가져온다.
 
-var categoryParam = new URL(url).searchParams.get("category"); //url에서 category라는 매개변수의 값을 가져온다
+let categoryParam = new URL(url).searchParams.get("category"); //url에서 category라는 매개변수의 값을 가져온다
 
-if (!categoryParam) { //전체
-    articleLink[0].classList.add('underline');
-} else if (categoryParam === '영화') {
-    articleLink[1].classList.add('underline');
-} else if (categoryParam === '게임') {
-    articleLink[2].classList.add('underline');
-} else if (categoryParam === '운동') {
-    articleLink[3].classList.add('underline');
-} else if (categoryParam === '산책') {
-    articleLink[4].classList.add('underline');
-} else if (categoryParam === '식사') {
-    articleLink[5].classList.add('underline');
-} else if (categoryParam === '만남') {
-    articleLink[6].classList.add('underline');
-} else if (categoryParam === '완료') {
-    articleLink[7].classList.add('underline');
+if (window.innerWidth >= 900) {
+    alert(categoryParam);
+    if (!categoryParam) { //전체
+        articleLink[0].classList.add('underline');
+    } else if (categoryParam === '영화') {
+        articleLink[1].classList.add('underline');
+    } else if (categoryParam === '게임') {
+        articleLink[2].classList.add('underline');
+    } else if (categoryParam === '운동') {
+        articleLink[3].classList.add('underline');
+    } else if (categoryParam === '산책') {
+        articleLink[4].classList.add('underline');
+    } else if (categoryParam === '식사') {
+        articleLink[5].classList.add('underline');
+    } else if (categoryParam === '만남') {
+        articleLink[6].classList.add('underline');
+    } else if (categoryParam === '완료') {
+        articleLink[7].classList.add('underline');
+    }
 }
 
 
-if(window.location.width <= 951) {
-    if (!categoryParam) { //전체
-        articleLink750px[0].classList.add('underline');
-    } else if (categoryParam === '영화') {
+if (window.location.width <= 900) {
+    alert(categoryParam);
+    if (categoryParam === '영화') {
         articleLink750px[1].classList.add('underline');
     } else if (categoryParam === '게임') {
         articleLink750px[2].classList.add('underline');
