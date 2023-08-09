8ef4fcc1cbb6558d9ef85f8efebfd11867a3fd1b
MinBumkim1901 <chngdch3@naver.com>
2023-08-09 11:43:31 +0900

    수정할거있으면 "연락주세요"

--- ProjectWeMeet/src/main/resources/static/resources/scripts/article.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/article.js
@@ -8,17 +8,17 @@ let categoryParam = new URL(url).searchParams.get("category"); //url에서 categ
 
     if (!categoryParam) { //전체
         articleLink[0].classList.add('underline');
-    } else if (categoryParam === '영화') {
-        articleLink[1].classList.add('underline');
-    } else if (categoryParam === '게임') {
-        articleLink[2].classList.add('underline');
     } else if (categoryParam === '운동') {
+        articleLink[1].classList.add('underline');
+    } else if (categoryParam === '푸드') {
+        articleLink[2].classList.add('underline');
+    } else if (categoryParam === '문화') {
         articleLink[3].classList.add('underline');
-    } else if (categoryParam === '산책') {
+    } else if (categoryParam === '취미') {
         articleLink[4].classList.add('underline');
-    } else if (categoryParam === '식사') {
+    } else if (categoryParam === '온라인') {
         articleLink[5].classList.add('underline');
-    } else if (categoryParam === '만남') {
+    } else if (categoryParam === '여행') {
         articleLink[6].classList.add('underline');
     } else if (categoryParam === '완료') {
         articleLink[7].classList.add('underline');
