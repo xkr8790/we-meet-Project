1b60357332a4943c0ca909c9a6b94c8f05caa240
MinBumkim1901 <chngdch3@naver.com>
2023-08-09 11:30:21 +0900

    수정할거있으면 "연락주세요"

--- ProjectWeMeet/src/main/resources/static/resources/scripts/article.js
+++ ProjectWeMeet/src/main/resources/static/resources/scripts/article.js
@@ -1,14 +1,11 @@
 const Article = document.getElementById('Article');
 const category = Article.querySelectorAll('.category-item');
 const articleLink = Article.querySelectorAll('.articleLink');
-const articleLink750px = Article.querySelectorAll('.articleLink750px')
 
 let url = window.location.href; //현재 컴퓨터가 실행되고있는 URL을 가져온다.
 
 let categoryParam = new URL(url).searchParams.get("category"); //url에서 category라는 매개변수의 값을 가져온다
 
-if (window.innerWidth >= 900) {
-    alert(categoryParam);
     if (!categoryParam) { //전체
         articleLink[0].classList.add('underline');
     } else if (categoryParam === '영화') {
@@ -26,24 +23,6 @@ if (window.innerWidth >= 900) {
     } else if (categoryParam === '완료') {
         articleLink[7].classList.add('underline');
     }
-}
 
 
-if (window.location.width <= 900) {
-    alert(categoryParam);
-    if (categoryParam === '영화') {
-        articleLink750px[1].classList.add('underline');
-    } else if (categoryParam === '게임') {
-        articleLink750px[2].classList.add('underline');
-    } else if (categoryParam === '운동') {
-        articleLink750px[3].classList.add('underline');
-    } else if (categoryParam === '산책') {
-        articleLink750px[4].classList.add('underline');
-    } else if (categoryParam === '식사') {
-        articleLink750px[5].classList.add('underline');
-    } else if (categoryParam === '만남') {
-        articleLink750px[6].classList.add('underline');
-    } else if (categoryParam === '완료') {
-        articleLink750px[7].classList.add('underline');
-    }
-}
+
